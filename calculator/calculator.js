85a4b6ab220605c2b6e3ef36683fb00bfab9c719
vkhanh0236 <vkhanh0236@mail.com>
2020-11-16 22:18:44 +0700

    calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,23 @@
+const numbers = document.getElementsByClassName("btn");
+const result = document.getElementById("result");
+
+for (let number of numbers) {
+  number.addEventListener("click", function () {
+    result.innerHTML += this.value;
+  });
+}
+
+function equal() {
+  let res = result.innerHTML;
+  let output = eval(res);
+  result.innerHTML = output;
+}
+
+function clean() {
+  result.innerHTML = " ";
+}
+
+function undo() {
+  let res = result.innerHTML;
+  result.innerHTML = res.substring(0, res.length - 1);
+}
