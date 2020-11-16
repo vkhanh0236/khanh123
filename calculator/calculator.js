00e16523a8a5ba3ef48d6fa1a76dab14db9e9f73
vkhanh0236 <vkhanh0236@mail.com>
2020-11-16 23:17:04 +0700

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,18 +6,40 @@ for (let number of numbers) {
     result.innerHTML += this.value;
   });
 }
-
+// tra ve ket qua tinh
 function equal() {
   let res = result.innerHTML;
   let output = eval(res);
   result.innerHTML = output;
 }
-
+//xoa toan bo
 function clean() {
   result.innerHTML = " ";
 }
-
+// xoa 1 so cuoi
 function undo() {
   let res = result.innerHTML;
   result.innerHTML = res.substring(0, res.length - 1);
+}
+// tinh binh phuong
+function binhphuong() {
+  let binhphuong= result.innerText;
+    binhphuong=binhphuong**2
+    result.innerHTML = binhphuong ;
+  
+}
+
+// function giaithua() {
+//     let giaithua=result.innerHTML;
+//     for (i = 1 ;0 < i < giaithua ; i++){
+//         giaithua=giaithua*i;
+//     }
+//     return result.innerHTML = giaithua;
+// }
+
+//tinh can bac 2
+function can(){
+    let can = result.innerText;
+    can=Math.sqrt(can)
+    result.innerHTML= can;
 }
