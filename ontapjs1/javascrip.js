1373713455eac8768851535fa94fd245fb24d856
vkhanh0236 <vkhanh0236@mail.com>
2020-12-22 09:40:06 +0700

    ontapjs1

--- ontapjs1/javascrip.js
+++ ontapjs1/javascrip.js
@@ -1,15 +1,17 @@
 //bai 1
 function maxString(arr){
     let maxstring = 0;
-    let arrmax= [] ;
+    let arrmax = [] ;
     for(let i = 0 ; i < arr.length ; i++){
-        if(maxstring <= arr[i].length){
-            maxstring = arr[i].length
-            arrmax.push(arr[i]) // thêm phần tử khi xuất hiện 1 chuỗi lớn nhất
+        if(maxstring < arr[i].length){
+            maxstring = arr[i].length;
         }
-        
     }
-    return arrmax
+    function check(maxstrings){
+        return maxstrings.length == maxstring;
+    }
+
+    return arrmax = arr.filter(check) 
 }
 // bai 2
 
@@ -30,9 +32,88 @@ function arrcomparison(arr1,arr2){
     return arr
 }
 // bai 3
+function arrziczac(m , n ){
+    let arr = [];
+    let x =1;
+    for(let k = 0; k< m; k++){
+        arr[k]=[];
+    }
+    for(let i= 0 ; i <= m ; i++){
+        if (i%2==0) {
+            
+        }
+        
+        
+    }
+
+    return arr
+}
+//bai3 copycode
+function vehinhkhohieu(m,n)
+        {
+            let mangMe = [];
+            let mangCon = [];
+            let mangKetQua = [];
+
+            for (let i=0; i<m*n; i++)
+                {
+                    mangMe.push(i+1);
+                }
+            
+            for (let y=0; y<m; y++)
+                {
+                    mangCon = mangMe.splice(0,n)
+
+                    if (y!=0 && y%2==1)
+                        {
+                            mangKetQua[y] = mangCon.reverse();
+                            // console.log(mangKetQua[y]);
+                        }
+                    else
+                        {
+                            mangKetQua[y] = mangCon;
+                            // console.log(mangKetQua[y]);
+                        }
+                }
+
+            return mangKetQua;
+        }
 
 
-
-
-
+// object
+//  let sv = [{ name:"Huy", gender: "Male", age: 20}, { name:"uya", gender: "Male", age: 100}]
 //bai4 
+function ageTb(arr){
+    let Tb = []
+    let tong = 0
+
+    for(let i = 0 ; i < arr.length ; i++){
+        Tb.push(arr[i]['age'])
+    }
+
+    for(let j of Tb ){
+        tong = tong + j
+        agetb = tong / 2
+    }
+    return agetb
+}
+// bai 5
+function sortAge(arr){
+    return arr.sort(function(a,b){return b.age - a.age})
+}
+//bai 6
+function sortName(arr){
+    return arr.sort(function(a, b){
+        var x = a.name.toLowerCase();
+        var y = b.name.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+      });
+}
+//bai 7
+function timTen(){
+    
+    return newArray= arr.filter(a=> (a.name.substring(0,1)=="H"|| a.name.substring(0,1)=="h"))
+    
+}
